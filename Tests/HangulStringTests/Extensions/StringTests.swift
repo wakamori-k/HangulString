71ed135129413acdf3bc9f1b73633647e889b7a0
wakamori-k <wakamorikazumasa@gmail.com>
2024-07-28 20:01:45 +0900

    Update romanize

--- Tests/HangulStringTests/Extensions/StringTests.swift
+++ Tests/HangulStringTests/Extensions/StringTests.swift
@@ -8,10 +8,9 @@ final class StringTests: XCTestCase {
     func testRomanized() {
         XCTAssertEqual("안녕하세요".romanized(), "an nyeong ha se yo")
 
-        // Expected failures
-        // XCTAssertEqual("안녕하세요?".romanized(), "an nyeong ha se yo?")
-        // XCTAssertEqual("안녕하세요.".romanized(), "an nyeong ha se yo.")
-
+        XCTAssertEqual("안녕하세요?".romanized(), "an nyeong ha se yo?")
+        XCTAssertEqual("안녕하세요.".romanized(), "an nyeong ha se yo.")
+        XCTAssertEqual("좋은 하루 보내세요".romanized(), "joh eun ha ru bo nae se yo")
     }
 
     func testRomanized_separator() {
@@ -23,6 +22,9 @@ final class StringTests: XCTestCase {
 
         XCTAssertEqual("아야우".romanized(separator: "-"), "a-ya-u")
         XCTAssertEqual("아야우".romanized(separator: "---"), "a---ya---u")
+
+        XCTAssertEqual("좋은 하루 보내세요".romanized(separator: ""), "joheun haru bonaeseyo")
+        XCTAssertEqual("좋은 하루 보내세요".romanized(separator: "-"), "joh-eun ha-ru bo-nae-se-yo")
     }
 
     func testRomanized_nonHangul() {
@@ -30,10 +32,9 @@ final class StringTests: XCTestCase {
         XCTAssertEqual("a".romanized(), "a")
         XCTAssertEqual("#".romanized(), "#")
 
-        // Expected failures
-        // XCTAssertEqual("あいう".romanized(), "あいう")
-        // XCTAssertEqual("abc".romanized(), "abc")
-        // XCTAssertEqual("#!?".romanized(), "#!?")
+        XCTAssertEqual("あいう".romanized(), "あいう")
+        XCTAssertEqual("abc".romanized(), "abc")
+        XCTAssertEqual("#!?".romanized(), "#!?")
     }
 
 }
