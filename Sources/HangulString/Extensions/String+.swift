247f285213af6492c9b3d9a135e8ba200deaae31
Kazumasa Wakamori <wakamorikazumasa@gmail.com>
2024-07-28 20:10:27 +0900

    feat: Update `String` extension (#2)
    - Add katakanize support
    - Update romanize to support non-hangul characters

--- Sources/HangulString/Extensions/String+.swift
+++ Sources/HangulString/Extensions/String+.swift
@@ -13,7 +13,55 @@ extension String {
     /// - Parameter separator: A string to be used as a separator
     /// - Returns: Romanized string
     func romanized(separator: String = " ") -> String {
-        let romans = self.map { $0.romanized() }
-        return romans.joined(separator: separator)
+        let splitted = split(text: self)
+        var result = ""
+        for subString in splitted {
+            if let hangul = try? HangulString(subString) {
+                result += hangul.romanized(separator: separator)
+            } else {
+                result += subString
+            }
+        }
+        
+        return result
+    }
+
+    /// Katakanize given hangul word by following the Revised Romanization of Korean rule.
+    /// - Returns: Katakanized string
+    func katakanize() -> String {
+        let splitted = split(text: self)
+        var result = ""
+        for subString in splitted {
+            if let hangul = try? HangulString(subString), let kana = try? hangul.katakanize() {
+                result += kana
+            } else {
+                result += subString
+            }
+        }
+        
+        return result
+    }
+    
+    /// Split `text` into an array of strings containing hangul and non-hangul parts respectively.
+    /// - Parameter text: A string to split
+    /// - Returns: An array of strings containing hangul and non-hangul parts respectively.
+    private func split(text: String) -> [String] {
+        var result: [String] = []
+        var currentString = ""
+        var isLastCharHangul: Bool? = nil
+        
+        for char in text {
+            let isHangul = char.isHangulJamos || char.isHangulSyllable
+            if let isLastCharHangul = isLastCharHangul, isHangul != isLastCharHangul {
+                result.append(currentString)
+                currentString = ""
+            }
+
+            currentString.append(char)
+            isLastCharHangul = isHangul
+        }
+        
+        result.append(currentString)
+        return result
     }
 }
--- Tests/HangulStringTests/Extensions/StringTests.swift
+++ Tests/HangulStringTests/Extensions/StringTests.swift
@@ -8,10 +8,9 @@ final class StringTests: XCTestCase {
     func testRomanized() {
         XCTAssertEqual("안녕하세요".romanized(), "an nyeong ha se yo")
 
-        // Expected failures
-        // XCTAssertEqual("안녕하세요?".romanized(), "an nyeong ha se yo?")
-        // XCTAssertEqual("안녕하세요.".romanized(), "an nyeong ha se yo.")
-
+        XCTAssertEqual("안녕하세요?".romanized(), "an nyeong ha se yo?")
+        XCTAssertEqual("안녕하세요.".romanized(), "an nyeong ha se yo.")
+        XCTAssertEqual("좋은 하루 보내세요".romanized(), "joh eun ha ru bo nae se yo")
     }
 
     func testRomanized_separator() {
@@ -23,6 +22,9 @@ final class StringTests: XCTestCase {
 
         XCTAssertEqual("아야우".romanized(separator: "-"), "a-ya-u")
         XCTAssertEqual("아야우".romanized(separator: "---"), "a---ya---u")
+
+        XCTAssertEqual("좋은 하루 보내세요".romanized(separator: ""), "joheun haru bonaeseyo")
+        XCTAssertEqual("좋은 하루 보내세요".romanized(separator: "-"), "joh-eun ha-ru bo-nae-se-yo")
     }
 
     func testRomanized_nonHangul() {
@@ -30,10 +32,30 @@ final class StringTests: XCTestCase {
         XCTAssertEqual("a".romanized(), "a")
         XCTAssertEqual("#".romanized(), "#")
 
-        // Expected failures
-        // XCTAssertEqual("あいう".romanized(), "あいう")
-        // XCTAssertEqual("abc".romanized(), "abc")
-        // XCTAssertEqual("#!?".romanized(), "#!?")
+        XCTAssertEqual("あいう".romanized(), "あいう")
+        XCTAssertEqual("abc".romanized(), "abc")
+        XCTAssertEqual("#!?".romanized(), "#!?")
+    }
+    
+    func testKatakanized() {
+        XCTAssertEqual("안녕하세요".katakanize(), "アンニョンハセヨ")
+
+        XCTAssertEqual("안녕하세요?".katakanize(), "アンニョンハセヨ?")
+        XCTAssertEqual("안녕하세요.".katakanize(), "アンニョンハセヨ.")
+
+        XCTAssertEqual("좋은 하루 보내세요".katakanize(), "チョッウン ハル ポネセヨ")
+        XCTAssertEqual("안녕히 계세요".katakanize(), "アンニョンヒ ケセヨ")
+        XCTAssertEqual("잘 지냈어요?".katakanize(), "チャ(ル) チネッオヨ?")
+    }
+
+    func testKatakanized_nonHangul() {
+        XCTAssertEqual("あ".katakanize(), "あ")
+        XCTAssertEqual("a".katakanize(), "a")
+        XCTAssertEqual("#".katakanize(), "#")
+
+        XCTAssertEqual("あいう".katakanize(), "あいう")
+        XCTAssertEqual("abc".katakanize(), "abc")
+        XCTAssertEqual("#!?".katakanize(), "#!?")
     }
 
 }
